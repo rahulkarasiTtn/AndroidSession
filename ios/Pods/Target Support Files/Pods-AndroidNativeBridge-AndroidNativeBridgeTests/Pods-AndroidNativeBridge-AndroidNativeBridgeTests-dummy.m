07927040753220c8ac0e36a1c9d79a9e441625ba
rahulKarasi <rahul.karasi@tothenew.com>
2024-02-04 16:43:11 +0530

    removed unwanted files

--- ios/Pods/Target Support Files/Pods-AndroidNativeBridge-AndroidNativeBridgeTests/Pods-AndroidNativeBridge-AndroidNativeBridgeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AndroidNativeBridge_AndroidNativeBridgeTests : NSObject
-@end
-@implementation PodsDummy_Pods_AndroidNativeBridge_AndroidNativeBridgeTests
-@end
